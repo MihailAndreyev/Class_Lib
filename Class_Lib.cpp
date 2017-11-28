beb783e0682f6590d5042c0396047a2d461b290a
MihailAndreyev <miwa.knx@gmail.com>
2017-11-28 22:56:43 +0300

    pre-Class is ready

--- Class_Lib.cpp
+++ Class_Lib.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 #include <string>
 
+class Lib
+{
+private:
+	int id_reader;
+	char name[20];
+	char surname[20];
+	int id_book;
+	char book[20];
+	char author[20];
+public:
+	void getdata();
+	void showdata()const;
+	int getID()const;
+};
+
+void Lib::getdata()
+{
+	std::cout<<"Prisvoyte ID chitatelu: ";
+	std::cin>>id_reader;
+	
+}
